51b9cf79d5961aebf06a6099f9b0d523550ca3cf
kluverto <kluverto12@gmail.com>
2026-02-06 11:37:26 +0100

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -68,13 +68,13 @@ app.get("/", (req, res) => {
 });
 
 const db = new Pool({
-  //connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: false
+  ssl: {rejectUnauthorized: false}
 })
 
 // GET chat history for a user
